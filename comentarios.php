b46920e0567b9709628e4b9cc55807f9649daf31
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-13 14:30:37 -0300

    comentários no php

---
+++ comentarios.php
@@ -0,0 +1,17 @@
+<?php
+
+// Comentário de uma linha!
+echo "Página de comentários!";
+# Comentário de uma linha!
+quebraLinha();
+quebraLinha();
+
+echo "Criando páginas com o PHP.";
+/*
+    Comentário de multiplas linhas!
+    Este tipo de comentário permite que varias
+    linhas sejam comentadas ao mesmo tempo.
+*/
+function quebraLinha() {
+    echo '<br>';
+}
