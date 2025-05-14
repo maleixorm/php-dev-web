cec1bd185b3add68248292e698154c9aeb944d37
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-14 15:18:48 -0300

    Trabalhando com constantes no php

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+function quebraLinha() {
+    echo "<br><br>";
+}
+
+define('BD_URL', 'localhost');
+define('BD_USER', 'php');
+define('BD_PASSWD', '123456');
+
+echo BD_URL;
+quebraLinha();
+echo BD_USER;
+quebraLinha();
+echo BD_PASSWD;
