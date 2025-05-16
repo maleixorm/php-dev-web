91fe532af396f7c2d1a836fb7ba5e951ba8149ac
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-16 10:40:21 -0300

    trabalhando com switch

---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$parametro = 4;
+
+switch ($parametro) {
+    case 1:
+        echo "entrou no case 1.";
+        break;
+    case 2:
+        echo "entrou no case 2.";
+        break;
+    case 3:
+        echo "entrou no case 3.";
+        break;
+    default:
+        echo "Entrou no padrão.";
+        break;
+}
