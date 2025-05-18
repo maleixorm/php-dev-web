031d798fec6d8b19ede19bb92a39db992fa5e736
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-18 18:36:00 -0300

    Manipulando datas e o timezone da página

--- funcoes_datas.php
+++ funcoes_datas.php
@@ -1,3 +1,18 @@
 <?php
 
-echo date('d/m/Y  H:i:s');
+function quebrarLinha() {
+    echo '<br>';
+}
+
+function separarLinha() {
+    echo '<hr>';
+}
+
+echo date('d/m/Y  H:i');
+separarLinha();
+echo date_default_timezone_get();
+separarLinha();
+date_default_timezone_set('America/Los_Angeles');
+echo date_default_timezone_get();
+separarLinha();
+echo date('d/m/Y - H:i');
