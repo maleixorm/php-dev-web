e5fe098494ff457bee438532a9058a01a7ea2c13
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-21 11:25:04 -0300

    Trabahando com estruturas condicionais

---
+++ loop_do_while.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 1;
+echo "-- Início do Loop -- <br>";
+do {
+    echo $a . '<br>';
+    $a++;
+} while ($a <= 10);
+echo "-- Fim do Loop -- <br>";
---
+++ loop_while.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 1;
+echo "-- Início do Loop -- <br>";
+while ($a <= 10) {
+    echo $a . '<br>';
+    $a++;
+}
+echo "-- Fim do Loop -- <br>";
