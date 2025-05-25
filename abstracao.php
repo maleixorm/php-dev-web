0485f241dbf94fa96df6936ec8b5c20b86b6e70d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-25 15:19:27 -0300

    Trabalhando com classes e abstração na orientação à objetos

---
+++ abstracao.php
@@ -0,0 +1,21 @@
+<?php
+
+class Funcionario {
+    public $nome = 'José';
+    public $telefone = '11 99999-8888';
+    public $numFilhos = 2;
+
+    function resumirCadFunc() {
+        return "$this->nome possui $this->numFilhos filho(s).<br>";
+    }
+
+    function modificarNumFilhos($numFilhos) {
+        $this->numFilhos = $numFilhos;
+    }
+}
+
+$y = new Funcionario();
+echo $y->resumirCadFunc();
+
+$y->modificarNumFilhos(3);
+echo $y->resumirCadFunc();
