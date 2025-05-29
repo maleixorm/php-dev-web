49258f8b9f33cc70135e5b94b2ee445afa5a568e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-29 19:03:52 -0300

    Trabalhando com diferentes namespaces

--- namespaces1.php
+++ namespaces1.php
@@ -2,32 +2,57 @@
 
 namespace A;
 
-class Cliente {
+class Cliente implements \A\Cadastro {
     public $nome;
     function __construct($nome)
     {
         $this->nome = $nome;
+        echo '<pre>';
+        print_r(get_class_methods($this));
+        echo '</pre>';
     }
     public function __get($attr)
     {
         $this->$attr;
     }
+    public function salvar()
+    {
+        echo 'Salvar';
+    }
+}
+
+interface Cadastro {
+    public function salvar();
 }
 
 namespace B;
 
-class Cliente {
+class Cliente implements \B\Cadastro {
     public $nome;
     function __construct($nome)
     {
         $this->nome = $nome;
+        echo '<pre>';
+        print_r(get_class_methods($this));
+        echo '</pre>';
     }
     public function __get($attr)
     {
         $this->$attr;
     }
+    public function remover()
+    {
+        echo 'Remover';
+    }
+}
+
+interface Cadastro {
+    public function remover();
 }
 
 $c = new \A\Cliente('John');
+$cl = new Cliente('James');
+
 print_r($c);
-$c->__get('nome');
+echo '<hr>';
+print_r($cl);
