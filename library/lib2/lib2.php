11f3821b8ee233b54494fe39e7abaa56676a7f42
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-30 01:13:03 -0300

    criando a segunda biblioteca

---
+++ library/lib2/lib2.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace B;
+
+class Cliente implements Cadastro {
+    public $nome;
+    
+    function __construct($nome)
+    {
+        $this->nome = $nome;
+        echo '<pre>';
+        print_r(get_class_methods($this));
+        echo '</pre>';
+    }
+    
+    public function __get($attr)
+    {
+        $this->$attr;
+    }
+    
+    public function remover()
+    {
+        echo 'Remover';
+    }
+    
+    public function salvar()
+    {
+        echo 'Salvar';
+    }
+}
+
+interface Cadastro {
+    public function remover();
+}
