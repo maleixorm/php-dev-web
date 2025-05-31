6fd1b45feff120a3ac636363464243ea546e2a3d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-31 02:58:36 -0300

    Lançando uma exception no código

--- tratamento_erros.php
+++ tratamento_erros.php
@@ -3,12 +3,21 @@
 try {
     echo '<h3> *** Try *** </h3>';
 
-    $sql = 'SELECT * FROM clientes';
-    mysql_query($sql);
+    // $sql = 'SELECT * FROM clientes';
+    // mysql_query($sql);
+
+    if(!file_exists("require_arquivo_a.php")) {
+        throw new Exception("O arquivo em questao deveria estar disponível as ". date('d/m/Y H:i:s') ." mas não estava. Vamos seguir mesmo assim!");   
+    }
     
 } catch (Error $err) {
-    echo '<h3> *** Catch *** </h3>';
+    echo '<h3> *** Catch Error *** </h3>';
     echo '<p style="color: red">' . $err . '</p>';
+    
+} catch (Exception $e) {
+    echo '<h3> *** Catch Exception *** </h3>';
+    echo '<p style="color: orange">' . $e . '</p>';
+    
 } finally {
-    echo '<h3> *** Finally *** </h3>';
+    echo '<h3> *** Finally *** </h3>'; // O Finally é uma opção opcional
 }
